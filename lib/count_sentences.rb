e07114d2a67de34160402f062a1741473ba0b967
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-06-20 13:10:10 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -14,9 +14,9 @@ class String
   def exclamation?
     self.end_with?("!")
   end
-
   def count_sentences
     self.split("?").count + self.split(".").count + self.split("!").count
+  binding.pry
   end
-  end
+  
 end
